5e4a8dc12f1001b75cde5a2a5d6884ea685fcb85
Shay <moeshakasanova@gmail.com>
2024-12-17 13:57:00 +0400

    changed app keys

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,9 +1,9 @@
 import { signInWithPopup } from 'firebase/auth';
-import { auth, googleAuthProvider } from './firebase-config'; 
+import { auth, googleAuthProvider} from './firebase-config';
 
 export const googleSignIn = async () => {
   try {
-    const result = await signInWithPopup(auth, googleAuthProvider); 
+    const result = await signInWithPopup(auth, googleAuthProvider);
     const user = result.user;
     console.log('Google Sign-In successful:', user);
     return user;
--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -3,13 +3,13 @@ import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAGJcLMp8YCvDLFigYWKhHp65haPDJ1y1w",
-    authDomain: "today-fitnessapp.firebaseapp.com",
-    projectId: "today-fitnessapp",
-    storageBucket: "today-fitnessapp.firebasestorage.app",
-    messagingSenderId: "245176161220",
-    appId: "1:245176161220:web:bf877060a25deb62720a91"
-};
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
+    };
 
 const app = initializeApp(firebaseConfig);
 
