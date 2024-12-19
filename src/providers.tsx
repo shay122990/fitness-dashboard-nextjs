a81d8818eb8da8a141c072a10930d91eabdc6c44
shay asanova <moeshakasanova@gmail.com>
2024-12-19 12:07:35 +0400

    redux-persist for local storage

--- src/providers.tsx
+++ src/providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 
-import { Provider } from 'react-redux';
-import store from '../src/store/index'; 
-
+import React from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "../src/store";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <Provider store={store}>
-      {children}
+      <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+        {children}
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,27 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; 
 import authReducer from "./authSlice";
 import workoutsReducer from "./workoutsSlice";
 import nutritionReducer from "./nutritionSlice";
 
+const persistConfig = {
+  key: "auth",
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
+
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
     workouts: workoutsReducer,
     nutrition: nutritionReducer,
   },
 });
 
+const persistor = persistStore(store);
+
+export { store, persistor };
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export default store;
