924035397c2c38ca1b0ffcc02879a78396877563
shay asanova <moeshakasanova@gmail.com>
2024-12-24 00:31:21 +0400

    Card - refactored for new props

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,14 +1,25 @@
 import { CardProps } from "../types/cardTypes";
-
-const Card: React.FC<CardProps> = ({ title, description, tabId, setActiveTab }) => {
+const Card: React.FC<CardProps> = ({ title, description, tabId, setActiveTab, actionButton }) => {
   return (
-    <div
-      className="border p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300 cursor-pointer"
-      onClick={() => setActiveTab(tabId)}
-    >
+    <div className="border p-4 rounded-lg shadow-md hover:shadow-lg transition duration-300 cursor-pointer">
       <h3 className="text-xl font-semibold mb-2">{title}</h3>
       <p className="text-white mb-4">{description}</p>
-      <span className="text-blue-500">Go to {title}</span>
+      {setActiveTab && (
+        <span
+          className="text-blue-500 cursor-pointer"
+          onClick={() => setActiveTab(tabId)}
+        >
+          Go to {title}
+        </span>
+      )}
+      {actionButton && (
+        <button
+          className="mt-2 px-4 py-2 bg-red-500 text-white rounded hover:bg-red-400"
+          onClick={actionButton.onClick}
+        >
+          {actionButton.label}
+        </button>
+      )}
     </div>
   );
 };
