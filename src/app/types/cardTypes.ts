e6b20247a0ccfd070b59280c4822771e4d49bb7f
shay asanova <moeshakasanova@gmail.com>
2024-12-26 11:55:59 +0400

    CardTypes - update of description type

--- src/app/types/cardTypes.ts
+++ src/app/types/cardTypes.ts
@@ -1,6 +1,8 @@
+import { ReactNode } from "react";
+
 export interface CardProps {
   title: string;
-  description: string;
+  description: ReactNode;
   tabId: string;
   setActiveTab?: (tabId: string) => void;
   actionButton?: {
