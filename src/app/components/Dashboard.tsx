0786646010564b954dac86b28a2d805f953d8104
shay asanova <moeshakasanova@gmail.com>
2025-01-23 13:08:43 +0400

    Dashboard - welcome back message if user is signed in

--- src/app/components/Dashboard.tsx
+++ src/app/components/Dashboard.tsx
@@ -1,31 +1,26 @@
 import Card from "./Card";
 import Image from "next/image";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store";
 
 interface DashboardProps {
   setActiveTab: (tabId: string) => void;
 }
 
 const Dashboard: React.FC<DashboardProps> = ({ setActiveTab }) => {
+  const user = useSelector((state: RootState) => state.auth.user);
   return (
     <div className="min-h-screen p-6">
       <Image src="/today-logo.png" width="100" height="100" alt="App Logo" className="w-20 mx-auto mb-4 animate-fadeIn rounded-full" />
-      
       <div className="bg-gradient-to-r from-blue-500 to-indigo-500 text-white p-6 rounded-lg text-center mb-8">
-        <h1 className="text-4xl font-bold mb-2 break-words">Welcome to Your Fitness Hub!</h1>
-        <p className="text-lg">Plan. Track. Achieve. All in one place.</p>
+        <h1 className="text-4xl font-bold mb-2 break-words">
+          {user ? `Welcome back ${user.name}` : "Welcome to Your Fitness Hub!"}
+        </h1>
+        <p className="text-lg">
+          {user ? "Let's plan your next workout." : "Plan. Track. Achieve. All in one place."}
+        </p>
       </div>
-
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
-        <div className="p-4 bg-green-100 rounded-lg shadow-md">
-          <h4 className="font-bold text-green-600">Workouts This Week</h4>
-          <p className="text-2xl font-semibold text-black">5</p>
-        </div>
-        <div className="p-4 bg-yellow-100 rounded-lg shadow-md">
-          <h4 className="font-bold text-yellow-600">Calories Tracked</h4>
-          <p className="text-2xl font-semibold text-black">12,000</p>
-        </div>
-      </div> */}
-
+      
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         <Card
           title="Workout Planner"
