979c9779395eabb521dcb201ac40007adaa7d6ca
shay asanova <moeshakasanova@gmail.com>
2025-02-18 15:18:52 +0400

    Modal - styling update

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,4 +1,5 @@
 import Button from "./Button";
+
 interface ModalProps {
   message: string;
   onClose: () => void;
@@ -9,15 +10,14 @@ const Modal: React.FC<ModalProps> = ({ message, onClose, visible }) => {
   if (!visible) return null;
 
   return (
-    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg">
-        <p className="text-xl text-blue-700">{message}</p>
+    <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 transition-opacity">
+      <div className="bg-white p-8 rounded-2xl shadow-xl w-80 text-center">
+        <p className="text-lg text-gray-800 font-semibold">{message}</p>
         <Button
-          className="mt-4 bg-blue-700 text-white py-2 px-4 rounded"
+          className="mt-6 bg-blue-950 hover:bg-blue-600 text-white py-2 px-6 rounded-lg transition-all duration-200"
           onClick={onClose}
           label="Close"
         />
-    
       </div>
     </div>
   );
