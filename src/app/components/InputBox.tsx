0352d5e31c2794f28b00b43859e5f814c2f80d44
shay asanova <moeshakasanova@gmail.com>
2025-02-20 15:55:09 +0400

    Layout, Input box- styling

--- src/app/components/InputBox.tsx
+++ src/app/components/InputBox.tsx
@@ -8,14 +8,14 @@ interface InputBoxProps {
 
 const InputBox: React.FC<InputBoxProps> = ({ label, placeholder, value, onChange, type = "text" }) => {
   return (
-    <div className="flex flex-col gap-1  w-full ">
+    <div className="flex flex-col gap-1 w-full ">
       <label className="text-green-400 font-medium text-sm">{label}</label>
       <input
         type={type}
         placeholder={placeholder}
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className="p-2 lg:p-3 rounded-lg bg-black bg-opacity-50 text-white placeholder:text-white  ease-in-out transform  shadow-md"
+        className="p-2 mb-2 lg:p-3 rounded-lg bg-black bg-opacity-50 text-white placeholder:text-white  ease-in-out transform  shadow-md"
       />
     </div>
   );
