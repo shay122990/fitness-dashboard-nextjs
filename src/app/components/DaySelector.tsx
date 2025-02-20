79aececd73b08b352957a62206996582f75018b1
shay asanova <moeshakasanova@gmail.com>
2025-02-20 15:30:03 +0400

    DaySelector- layout styling

--- src/app/components/DaySelector.tsx
+++ src/app/components/DaySelector.tsx
@@ -5,15 +5,15 @@ interface DaySelectorProps {
 }
 
 const DaySelector: React.FC<DaySelectorProps> = ({ selectedDay, onChange, days }) => (
-  <div className="flex items-center gap-4">
-    <label htmlFor="day" className="font-semibold text-lg">
+  <div className="flex items-center justify-between mb-6">
+    <label htmlFor="day" className="font-semibold lg:text-xl uppercase">
       Select Day:
     </label>
     <select
       id="day"
       value={selectedDay}
       onChange={(e) => onChange(e.target.value)}
-      className="px-3 py-2 border-gray-300 rounded-lg bg-black bg-opacity-50 text-white shadow-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-950 hover:bg-blue-950"
+      className="px-3 py-1 border-gray-300 rounded-lg bg-black bg-opacity-50 text-white shadow-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-950 hover:bg-blue-950"
     >
       {days.map((day) => (
         <option key={day} value={day} className="text-black">
