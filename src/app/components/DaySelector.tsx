84c83b11fd89c823ce0567e9f0bba0c775430dad
shay asanova <moeshakasanova@gmail.com>
2025-03-20 14:05:48 +0400

    DaySelector - border added for clear view

--- src/app/components/DaySelector.tsx
+++ src/app/components/DaySelector.tsx
@@ -13,7 +13,7 @@ const DaySelector: React.FC<DaySelectorProps> = ({ selectedDay, onChange, days }
       id="day"
       value={selectedDay}
       onChange={(e) => onChange(e.target.value)}
-      className="px-3 py-1 border-gray-300 rounded-lg bg-black bg-opacity-50 text-white shadow-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-950 hover:bg-blue-950"
+      className="px-3 py-1 border border-green-500 rounded-lg bg-black bg-opacity-50 text-white shadow-md transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-950 hover:bg-blue-950"
     >
       {days.map((day) => (
         <option key={day} value={day} className="text-black">
