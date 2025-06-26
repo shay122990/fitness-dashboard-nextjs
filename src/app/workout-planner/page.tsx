cf46a98f62e4a9a2cce292b41c759002349df646
shay asanova <moeshakasanova@gmail.com>
2025-06-27 01:52:31 +0400

    accessibility heading tree workout planner page

--- src/app/workout-planner/page.tsx
+++ src/app/workout-planner/page.tsx
@@ -1,12 +1,22 @@
-"use client"
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../firebase/firebase-config";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState } from "@/store/store";
-import {addWorkout,removeWorkout,updateWorkout,setWorkouts,} from "@/store/workoutsSlice";
-import {fetchUserWorkouts,saveWorkout,removeWorkoutFromFirestore,updateWorkoutInFirestore,} from "../../firebase/firestore";
+import {
+  addWorkout,
+  removeWorkout,
+  updateWorkout,
+  setWorkouts,
+} from "@/store/workoutsSlice";
+import {
+  fetchUserWorkouts,
+  saveWorkout,
+  removeWorkoutFromFirestore,
+  updateWorkoutInFirestore,
+} from "../../firebase/firestore";
 import { daysOfWeek } from "../utils/days";
 import InputBox from "../components/InputBox";
 import DaySelector from "../components/DaySelector";
@@ -14,7 +24,7 @@ import Button from "../components/Button";
 import Card from "../components/Card";
 import AuthCheck from "../components/AuthCheck";
 import { v4 as uuidv4 } from "uuid";
-import Modal from "../components/Modal"; 
+import Modal from "../components/Modal";
 
 const Planner = () => {
   const dispatch = useDispatch();
@@ -28,7 +38,7 @@ const Planner = () => {
   const [editingWorkout, setEditingWorkout] = useState<string | null>(null);
   const [userId, setUserId] = useState<string | null>(null);
   const [authLoading, setAuthLoading] = useState<boolean>(true);
-  const [modalVisible, setModalVisible] = useState(false); 
+  const [modalVisible, setModalVisible] = useState(false);
   const [modalMessage, setModalMessage] = useState("");
 
   useEffect(() => {
@@ -64,25 +74,43 @@ const Planner = () => {
   const addOrUpdateWorkoutHandler = async () => {
     const repsPattern = /^\d+(-\d+)?$/;
     if (!repsPattern.test(reps)) {
-      setModalMessage("Please enter reps as a single number (e.g., 8) or a range (e.g., 8-10).");
+      setModalMessage(
+        "Please enter reps as a single number (e.g., 8) or a range (e.g., 8-10)."
+      );
       setModalVisible(true);
       return;
     }
 
     const weightPattern = /^(\d+(\.\d+)?(kg|lbs)?)|([a-zA-Z\s]+)$/;
     if (!weightPattern.test(weight.toString())) {
-      setModalMessage("Please enter weight as a number (e.g., 10kg, 10) or a text (e.g., pink band).");
+      setModalMessage(
+        "Please enter weight as a number (e.g., 10kg, 10) or a text (e.g., pink band)."
+      );
       setModalVisible(true);
       return;
     }
 
     if (selectedDay && newWorkout.trim() && sets.trim()) {
-      const capitalizeWords = (text: string) => text.replace(/\b\w/g, (char) => char.toUpperCase());
-      const workoutDetails = `${capitalizeWords(newWorkout.trim())} - Sets: ${sets}, Reps: ${reps}, Weight: ${weight}`;
-            if (editingWorkout) {
-        dispatch(updateWorkout({ day: selectedDay, oldWorkout: editingWorkout, newWorkout: workoutDetails }));
+      const capitalizeWords = (text: string) =>
+        text.replace(/\b\w/g, (char) => char.toUpperCase());
+      const workoutDetails = `${capitalizeWords(
+        newWorkout.trim()
+      )} - Sets: ${sets}, Reps: ${reps}, Weight: ${weight}`;
+      if (editingWorkout) {
+        dispatch(
+          updateWorkout({
+            day: selectedDay,
+            oldWorkout: editingWorkout,
+            newWorkout: workoutDetails,
+          })
+        );
         if (userId) {
-          await updateWorkoutInFirestore(userId, selectedDay, editingWorkout, workoutDetails);
+          await updateWorkoutInFirestore(
+            userId,
+            selectedDay,
+            editingWorkout,
+            workoutDetails
+          );
         }
         setEditingWorkout(null);
       } else {
@@ -123,7 +151,11 @@ const Planner = () => {
     }
   };
 
-  const renderCardForDay = (day: string, workouts: string[], showActions: boolean = false) => (
+  const renderCardForDay = (
+    day: string,
+    workouts: string[],
+    showActions: boolean = false
+  ) => (
     <Card
       key={`${day}-${uuidv4()}`}
       title={`${day} Workouts`}
@@ -142,8 +174,16 @@ const Planner = () => {
                 </div>
                 {showActions && (
                   <div className="flex gap-2">
-                    <Button label="Edit" className="text-blue-200" onClick={() => startEditingWorkout(workout)} />
-                    <Button label="Remove" className="text-red-300" onClick={() => removeWorkoutHandler(day, workout)} />
+                    <Button
+                      label="Edit"
+                      className="text-blue-200"
+                      onClick={() => startEditingWorkout(workout)}
+                    />
+                    <Button
+                      label="Remove"
+                      className="text-red-300"
+                      onClick={() => removeWorkoutHandler(day, workout)}
+                    />
                   </div>
                 )}
               </li>
@@ -156,11 +196,10 @@ const Planner = () => {
       textColor="text-white"
     />
   );
-  
 
   const closeModal = () => {
     setModalVisible(false);
-    setModalMessage(""); 
+    setModalMessage("");
   };
 
   return (
@@ -171,22 +210,49 @@ const Planner = () => {
       onRedirect={() => (window.location.href = "/profile")}
       message="Sign in to save your workouts."
     >
-        <div className="mb-24 p-4 bg-black bg-opacity-30 rounded">
+      <div className="mb-24 p-4 bg-black bg-opacity-30 rounded">
         <div className="mb-3 bg-transparent-20 backdrop-blur-md rounded text-center p-4 shadow-lg">
-          <h3 className="text-xl font-bold uppercase text-green-400 drop-shadow-md">
+          <h1 className="text-xl font-bold uppercase text-green-400 drop-shadow-md">
             Workout Planner
-          </h3>
-          <h4 className="drop-shadow-sm">
+          </h1>
+          <h2 className="drop-shadow-sm">
             Plan your weekly workout and keep track for progressive overload
-          </h4>
+          </h2>
         </div>
         <div className="flex flex-col lg:flex-row justify-between gap-5">
           <div className="w-full sm:w-full lg:w-1/2 flex flex-col justify-between gap-1">
-            <DaySelector selectedDay={selectedDay} onChange={setSelectedDay} days={daysOfWeek} />
-            <InputBox label="WORKOUT NAME" placeholder="e.g., Squats" value={newWorkout} onChange={setNewWorkout} />
-            <InputBox label="SETS" placeholder="e.g., 3" value={sets} type="text" onChange={setSets} />
-            <InputBox label="REPS" placeholder="e.g., 8 or 8-10" value={reps} type="text" onChange={setReps} />
-            <InputBox label="WEIGHT" placeholder="e.g., 10kg or band" value={weight.toString()} type="text" onChange={setWeight} />
+            <DaySelector
+              selectedDay={selectedDay}
+              onChange={setSelectedDay}
+              days={daysOfWeek}
+            />
+            <InputBox
+              label="WORKOUT NAME"
+              placeholder="e.g., Squats"
+              value={newWorkout}
+              onChange={setNewWorkout}
+            />
+            <InputBox
+              label="SETS"
+              placeholder="e.g., 3"
+              value={sets}
+              type="text"
+              onChange={setSets}
+            />
+            <InputBox
+              label="REPS"
+              placeholder="e.g., 8 or 8-10"
+              value={reps}
+              type="text"
+              onChange={setReps}
+            />
+            <InputBox
+              label="WEIGHT"
+              placeholder="e.g., 10kg or band"
+              value={weight.toString()}
+              type="text"
+              onChange={setWeight}
+            />
             <Button
               label={editingWorkout ? "Update Workout" : "Add Workout"}
               onClick={addOrUpdateWorkoutHandler}
@@ -194,21 +260,31 @@ const Planner = () => {
             />
           </div>
           <div className="w-full lg:w-1/2">
-            <h4 className="text-green-400 uppercase bg-gray-900 text-center p-6 rounded mb-2">
+            <h3 className="text-green-400 uppercase bg-gray-900 text-center p-6 rounded mb-2">
               Workouts for {selectedDay}
-            </h4>
-            <div className="mt-4">{renderCardForDay(selectedDay, workouts[selectedDay] || [], true)}</div>
+            </h3>
+            <div className="mt-4">
+              {renderCardForDay(selectedDay, workouts[selectedDay] || [], true)}
+            </div>
           </div>
         </div>
         <div className="mt-6">
-          <h4 className="mt-2 text-green-400 uppercase bg-gray-900 text-center p-6 rounded mb-2">Workouts for the Week</h4>
+          <h3 className="mt-2 text-green-400 uppercase bg-gray-900 text-center p-6 rounded mb-2">
+            Workouts for the Week
+          </h3>
           <div className="mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-            {daysOfWeek.map((day) => renderCardForDay(day, workouts[day] || []))}
+            {daysOfWeek.map((day) =>
+              renderCardForDay(day, workouts[day] || [])
+            )}
           </div>
         </div>
       </div>
-      
-      <Modal message={modalMessage} onClose={closeModal} visible={modalVisible} />
+
+      <Modal
+        message={modalMessage}
+        onClose={closeModal}
+        visible={modalVisible}
+      />
     </AuthCheck>
   );
 };
