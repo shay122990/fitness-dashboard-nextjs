98787581149e788e62103e96beaff8eea136d306
shay asanova <moeshakasanova@gmail.com>
2025-07-22 08:42:49 +0400

    profile page edit, watertracker added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -10,6 +10,7 @@ import { googleSignIn } from "../../firebase/auth";
 import Image from "next/image";
 import Button from "../components/Button";
 import { useRouter } from "next/navigation";
+import WaterTracker from "./components/WaterTracker";
 
 const Profile = () => {
   const dispatch = useDispatch();
@@ -41,43 +42,48 @@ const Profile = () => {
 
   return (
     <div
-      className="relative flex justify-center  h-full px-6 pb-4 pt-20 rounded bg-cover bg-center bg-no-repeat"
+      className="relative flex justify-center  h-full px-6 py-10 rounded bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: "url('/profile-bg.jpg')" }}
     >
       <div className="absolute inset-0 bg-gray-950 bg-opacity-70"></div>
-      <div className="relative flex flex-col bg-white shadow-md rounded-lg p-4 text-center justify-center items-center w-full h-80 bg-gradient-to-br from-gray-900 to-blue-700">
-        <h1 className="text-2xl font-bold text-center mb-4">Your Profile</h1>
-        {user ? (
-          <>
-            <div className="text-white">
-              <Image
-                src={user.photoURL || "/default-profile.png"}
-                alt={user.name || "Profile Picture"}
-                className="w-24 h-24 rounded-full mx-auto border-2 border-gray-300 mb-4"
-                width={96}
-                height={96}
+      <div className="relative flex flex-col gap-8 shadow-md rounded-lg p-4 text-center  items-center w-full bg-blue-900/15">
+        <div className="w-full rounded bg-black/55 py-10">
+          <h1 className="text-2xl font-bold text-center mb-4">Your Profile</h1>
+          {user ? (
+            <>
+              <div className="text-white">
+                <Image
+                  src={user.photoURL || "/default-profile.png"}
+                  alt={user.name || "Profile Picture"}
+                  className="w-24 h-24 rounded-full mx-auto border-2 border-gray-300 mb-4"
+                  width={96}
+                  height={96}
+                />
+                <p className="mb-2">
+                  <span className="font-semibold">Name:</span> {user.name}
+                </p>
+                <p className="mb-2 text-sm break-words">
+                  <span className="font-semibold">Email:</span> {user.email}
+                </p>
+              </div>
+              <Button
+                label="Log Out"
+                onClick={handleLogout}
+                className="w-52 bg-red-500"
               />
-              <p className="mb-2">
-                <span className="font-semibold">Name:</span> {user.name}
-              </p>
-              <p className="mb-2 text-sm break-words">
-                <span className="font-semibold">Email:</span> {user.email}
-              </p>
-            </div>
+            </>
+          ) : (
             <Button
-              label="Log Out"
-              onClick={handleLogout}
-              className="w-52 bg-red-500"
+              label={loading ? "Signing in..." : "Sign in with Google"}
+              onClick={handleSignIn}
+              disabled={loading}
+              className="w-52 bg-green-500"
             />
-          </>
-        ) : (
-          <Button
-            label={loading ? "Signing in..." : "Sign in with Google"}
-            onClick={handleSignIn}
-            disabled={loading}
-            className="w-52 bg-green-500"
-          />
-        )}
+          )}
+        </div>
+        <div className="w-full flex flex-row items-start">
+          <WaterTracker />
+        </div>
       </div>
     </div>
   );
